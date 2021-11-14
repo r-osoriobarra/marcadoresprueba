9ed9b535dcc415b3839c001ec89ee92b9b8a1145
rodrigo <rob.developer@protonmail.com>
2021-11-14 13:01:13 -0300

    add enpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-resources :bookmarks
-resources :url_types
-resources :categories
   root "bookmarks#index"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :bookmarks
+  resources :url_types
+  resources :categories
+
+  namespace :api, defaults: {format: 'json'} do
+    namespace :v1 do
+      get 'category/:id', to: 'endpoints#api'
+    end
+  end  
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,40 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Category.destroy_all
 
-# Faker::TvShows::SouthPark.quote
+#create main categories
+3.times do
+    Category.create(
+        name: Faker::Music::RockBand.unique.name
+    )
+end
+
+#create sub_categories
+10.times do
+    Category.create(
+        name: Faker::Music::RockBand.unique.song,
+        category_id: rand(1..3)
+    )
+end
+
+#create url_types
+10.times do
+    UrlType.create(
+        name: Faker::Music.unique.genre
+    )
+end
+
+#create bookmars
+20.times do
+    Bookmark.create(
+        name: Faker::Music.unique.album,
+        url: Faker::Internet.unique.url,
+        category_id: rand(1..13),
+        url_type_id: rand(1..10)
+    )
+end
+
+
+
 
